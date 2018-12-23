58957692a4ff19a79852d12e25867fb338481890
Khalid MAJDOUB <majdoub.khalid@gmail.com>
2018-12-23 03:57:04 +0100

    Update main.ml

--- Parsing/main.ml
+++ Parsing/main.ml
@@ -3,31 +3,23 @@ open Lexer
 open Parser
 
 
-(*
-let rec examine_all lexbuf =
-let res = Parser.prog Lexer.read lexbuf in
-	
-match  res with
-  | Some value -> Java.print_lexeme value; print_string " "; examine_all lexbuf 
-  | None -> ()
-*)
 
 let compile file =
-		print_string ("File "^file^" is being treated!\n");
-		try
-				let input_file = open_in file in
-				let lexbuf = Lexing.from_channel input_file in
-						try
-								prog read lexbuf; 
-						with
-								| Error (kind, debut, fin) -> 
-										close_in (input_file);
-										report_error kind; 
-										print_position debut fin;                                                                                                                 
-					print_newline ();
-					close_in (input_file);
-					exit 0
-		with Sys_error s ->
-			print_endline ("Can’t find file ’" ^ file ^ "’")
-let _ = Arg.parse [] compile ""
+print_string ("File "^file^" is being treated!\n");
+try
+	let input_file = open_in file in
+	let lexbuf = Lexing.from_channel input_file in
+	try
+		let javaCode = Parser.prog Lexer.read lexbuf in print_javaCode javaCode;
+		print_newline ();
+		close_in (input_file);
+	with
+	| Errord (kind, debut, fin) -> 
+		close_in (input_file);
+		report_error kind; 
+		print_position debut fin;                                                                                            
+		print_newline ();
+		close_in (input_file);
 
+with Sys_error s -> print_endline ("Can’t find file ’" ^ file ^ "’")
+let _ = Arg.parse [] compile ""
