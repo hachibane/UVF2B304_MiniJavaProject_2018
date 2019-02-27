17e16863031313c829aa78df658edf329f8353c9
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-02-27 22:18:48 +0100

    adding first types checking in Compile.ml

--- Phase2/Main/Compile.ml
+++ Phase2/Main/Compile.ml
@@ -7,9 +7,14 @@ let execute lexbuf verbose =
     TypeAST.type_program ast;
     if verbose then AST.print_program ast
   with
+    | CheckAST.Wrong_types_aop(x, operator, y) ->
+      print_string ("The operator " ^ (AST.string_of_assign_op operator));
+      print_string ("needs two values of the same type");
+      print_string ("but he had : " ^ (CheckAST.stringOf_prim x));
+      print_endline ("and : " ^ (CheckAST.stringOf_prim y))
     | Error ->
       print_string "Syntax error: ";
       Location.print (Location.curr lexbuf)
-    | Error.Error(e,l) ->
-      Error.report_error e;
-      Location.print l
+    | Error.Error(err,loc) ->
+      Error.report_error err;
+      Location.print loc
