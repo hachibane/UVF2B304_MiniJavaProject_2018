db8ca1f8643d1e3d748bf030cdf786e16c32ca5f
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-02-27 21:17:16 +0100

    update Compile using ast_type

--- Phase2/Main/Compile.ml
+++ Phase2/Main/Compile.ml
@@ -1,11 +1,13 @@
 open Parser
 
-let execute lexbuf verbose = 
-  try 
+let execute lexbuf verbose =
+  try
     let ast = compilationUnit Lexer.token lexbuf in
     print_endline "successfull parsing";
-    if verbose then AST.print_program ast 
-  with 
+    (*if verbose then AST.print_program ast;*)
+    let ast_type = TypeAST.type_program ast in
+    if verbose then AST.print_program ast_type
+  with
     | Error ->
       print_string "Syntax error: ";
       Location.print (Location.curr lexbuf)
