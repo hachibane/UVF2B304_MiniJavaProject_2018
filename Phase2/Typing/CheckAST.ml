754dbab1d6f27293c0b6cb755319d05c2fa48fda
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-03-01 15:22:33 +0100

    Working on Typing : operation , unitary , post ans pre op

--- Phase2/Typing/CheckAST.ml
+++ Phase2/Typing/CheckAST.ml
@@ -1,39 +1,45 @@
 open AST
 
 (*here are defined some auxiliary functions *)
-let stringOf_prim = function
-  | Some(Type.Primitive(Type.Int))           -> "int"
-  | Some(Type.Primitive(Type.Int))           -> "long"
-  | Some(Type.Primitive(Type.Byte))          -> "byte"
-  | Some(Type.Primitive(Type.Char))          -> "char"
-  | Some(Type.Primitive(Type.Short))         -> "short"
-  | Some(Type.Primitive(Type.Boolean))       -> "boolean"
-  | Some(Type.Primitive(Type.Float))         -> "float"
-  | Some(Type.Ref({tpath=[]; tid="String"})) -> "string"
+let string_of_prefix_type = function
+  | Op_not -> "boolean"
+  | Op_bnot -> "int"
+  | Op_neg | Op_incr | Op_decr | Op_plus -> "int ou float"
 
 (* Here are defined the exceptions *)
 exception Wrong_type_bool of Type.t option
 exception Wrong_types_bool of Type.t option * Type.t option
 exception Wrong_type_if of Type.t option
+exception Wrong_type_post of Type.t option
 exception Wrong_types_operation  of Type.t option * infix_op * Type.t option
 exception Wrong_types_assign_operation of Type.t option * assign_op * Type.t option
-
+exception Wrong_type_unitary_operation of prefix_op * Type.t option
 
 (*Here are defined helping exception messages *)
 let print_wrong_types_operation x operator y = 
-	print_endline("wrong types : " ^ (stringOf_prim x) ^ " different from : " ^ (stringOf_prim y))
+	print_string("Operator : " ^ (AST.string_of_infix_op operator));
+	print_endline("wrong types : " ^ (Type.stringOfOpt x) ^ " different from : " ^ (Type.stringOfOpt y))
 
 let print_wrong_types_assign_operation x operator y =
-	print_endline("wrong types : " ^ (stringOf_prim x) ^ " different from : " ^ (stringOf_prim y))
+	print_string("Operator : " ^ (AST.string_of_assign_op operator));
+	print_endline("wrong types : " ^ (Type.stringOfOpt x) ^ " different from : " ^ (Type.stringOfOpt y))
 
 let print_wrong_type_if elem = 
-	print_endline((stringOf_prim elem) ^ " is not a boolean needed by the if condition")
+	print_endline((Type.stringOfOpt elem) ^ " is not a boolean needed by the if condition")
 
 let print_wrong_type_bool elem = 
-	print_endline("Wrong type : " ^ (stringOf_prim elem) ^ "should be a boolean")
+	print_endline("Wrong type : " ^ (Type.stringOfOpt elem) ^ "should be a boolean")
 
 let print_wrong_types_bool elem1 elem2 = 
-	print_endline("wrong types : " ^ (stringOf_prim elem1) ^ " different from : " ^ (stringOf_prim elem2))
+	print_endline("wrong types : " ^ (Type.stringOfOpt elem1) ^ " different from : " ^ (Type.stringOfOpt elem2))
+
+let print_wrong_type_unitary_operation operator x =
+	print_string("Operator : " ^ (AST.string_of_prefix_op operator));
+	print_string("Expects : " ^ (string_of_prefix_type operator));
+	print_string("Gots : " ^ (Type.stringOfOpt x))
+
+let print_wrong_type_post elem =
+	print_endline(" ++ and -- do not expect a " ^ (Type.stringOfOpt elem) ^ "type object")
 
 (* Here are defined the verifications *)
 let verify_assign_operation_type x operator y =
@@ -44,7 +50,22 @@ let verify_operation_type x operator y =
 
 let verify_tern_type elem x y =
   if elem <> Some(Type.Primitive(Type.Boolean)) then raise(Wrong_type_bool(elem));
-  if x <> y then raise(Wrong_types_bool(x, y))
+  match x, y with
+  | Some(Type.Primitive(_)), None -> raise(Wrong_types_bool(x, y))
+  | None, Some(Type.Primitive(_)) -> raise(Wrong_types_bool(x, y))
+  | Some(_), None -> ()
+  | None, Some(_) -> ()
+	| Some(typ1), Some(typ2) -> if typ1 <> typ2 then raise(Wrong_types_bool(x, y))
 
 let verify_if_type elem =
-	if elem <> Some(Type.Primitive(Type.Boolean)) then raise(Wrong_type_if(elem));
+	if elem <> Some(Type.Primitive(Type.Boolean)) then raise(Wrong_type_if(elem))
+
+let verify_post_type elem =
+	if (elem <> Some(Type.Primitive(Type.Int)) && elem <> Some(Type.Primitive(Type.Float))) then raise(Wrong_type_post(elem))
+
+let verify_pre_type operator elem =
+	match operator with
+	| Op_not  -> if elem <> Some(Type.Primitive(Type.Boolean)) then raise(Wrong_type_unitary_operation(operator, elem))
+	| Op_bnot -> if elem <> Some(Type.Primitive(Type.Int)) then raise(Wrong_type_unitary_operation(operator, elem))
+	| Op_neg | Op_incr | Op_decr | Op_plus -> 
+		if (elem <> Some(Type.Primitive(Type.Int)) && elem <> Some(Type.Primitive(Type.Float))) then raise(Wrong_type_unitary_operation(operator, elem))
--- Phase2/Typing/TypeAST.ml
+++ Phase2/Typing/TypeAST.ml
@@ -13,18 +13,34 @@ let type_val v =
 (*match the type of the expression following the JavaDoc specification*)
 let rec type_expression expr =
   match expr.edesc with
-  | Pre(operation, exp)              -> type_expression exp; expr.etype <- exp.etype
-  | Post(exp, operation)             -> type_expression exp; expr.etype <- exp.etype
+  | Pre(operation, exp)              -> type_expression exp; 
+                                        CheckAST.verify_pre_type operation exp.etype;
+                                        expr.etype <- exp.etype
+  | Post(exp, operation)             -> type_expression exp;
+                                        CheckAST.verify_post_type exp.etype;
+                                        expr.etype <- exp.etype
   | Cast(tpe,expr)                   -> type_expression expr; expr.etype <- Some(tpe)
   | Instanceof(expr, tpe)            -> type_expression expr
   | Val v                            -> expr.etype <- type_val v
   | Type tpe                         -> expr.etype <- Some(tpe)
   | ClassOf tpe                      -> expr.etype <- Some(tpe)
-  | AssignExp(expr1,operation,expr2) -> type_expression expr1; type_expression expr2; CheckAST.verify_assign_operation_type expr1.etype operation expr2.etype
-  | Op(expr1, operation, expr2)      -> type_expression expr1; type_expression expr2; CheckAST.verify_operation_type expr1.etype operation expr2.etype; expr.etype <- expr1.etype
-  | If(expr1, expr2, expr3)          -> type_expression expr1; type_expression expr2; type_expression expr3; CheckAST.verify_if_type expr1.etype
-  | CondOp(expr1, expr2, expr3)      -> type_expression expr1; type_expression expr2; type_expression expr3; CheckAST.verify_tern_type expr1.etype expr2.etype expr3.etype; expr.etype <- expr2.etype
+  | AssignExp(expr1,operation,expr2) -> type_expression expr1; type_expression expr2; 
+                                        CheckAST.verify_assign_operation_type expr1.etype operation expr2.etype;
+                                        expr.etype <- expr1.etype
+  | Op(expr1, operation, expr2)      -> type_expression expr1; type_expression expr2; 
+                                        CheckAST.verify_operation_type expr1.etype operation expr2.etype;
+                                        (match operation with
+                                        | Op_cor | Op_cand
+                                        | Op_eq | Op_ne | Op_gt | Op_lt | Op_ge | Op_le -> expr.etype <- Some(Type.Primitive(Type.Boolean))
+                                        | Op_or | Op_and | Op_xor
+                                        | Op_shl | Op_shr | Op_shrr
+                                        | Op_add | Op_sub | Op_mul | Op_div | Op_mod -> expr.etype <- expr1.etype)
   | VoidClass                        -> ()
+  | If(expr1, expr2, expr3)          -> type_expression expr1; type_expression expr2; type_expression expr3; 
+                                        CheckAST.verify_if_type expr1.etype
+  | CondOp(expr1, expr2, expr3)      -> type_expression expr1; type_expression expr2; type_expression expr3;     
+                                        CheckAST.verify_tern_type expr1.etype expr2.etype expr3.etype; 
+                                        if expr2.etype <> None then expr.etype <- expr2.etype else expr.etype <- expr3.etype
 
 (*typing for a statement*)
 let rec type_statement statement =
