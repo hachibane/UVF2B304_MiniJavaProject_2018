6d2a1a228f316a55b32c7b105291442061739994
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-03-02 10:42:39 +0100

    tests

---
+++ Phase2/tests/First_Typing_Test.java
@@ -0,0 +1,27 @@
+package robin;
+
+public class A {
+    int test() {
+        0 = 1;
+
+
+        ++true;
+        ++4;
+
+        if (true) {
+            5 = 4;
+        }
+
+        if ("tomate") {
+            4 + 8;
+        }
+
+        if (4 + 5 = "toto") {
+            0 = 1;
+        }
+
+         (true? null: 0);
+
+        return 34;
+    }
+}
