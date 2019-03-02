8e39f82d8428926b9207c6d136be6c6128e37573
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-03-02 10:45:58 +0100

    tests array

--- Phase2/tests/First_Typing_Test.java
+++ Phase2/tests/First_Typing_Test.java
@@ -1,27 +1,13 @@
 package robin;
 
-public class A {
-    int test() {
-        0 = 1;
-
-
-        ++true;
-        ++4;
-
-        if (true) {
-            5 = 4;
-        }
-
-        if ("tomate") {
-            4 + 8;
-        }
-
-        if (4 + 5 = "toto") {
-            0 = 1;
-        }
-
-         (true? null: 0);
-
-        return 34;
+public class A 
+{
+    int test() 
+    {
+        int[] test_array    = new int[8];
+        int[] test_array2   = {1,2,3,4,5,6,7,8};
+        int[] test_array3   = new int[]{1,2,3,4,5,6,7,8};
+        float[] test_array4 = {0.1, 0.2, 0.3, 0.4};
+        return 8;
     }
 }
