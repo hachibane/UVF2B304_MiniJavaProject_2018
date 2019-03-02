c96234ecf1838c46c3ad87a04f20c3c01a4862e7
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-03-02 16:14:15 +0100

    update compile with exceptions

--- Phase2/Main/Compile.ml
+++ Phase2/Main/Compile.ml
@@ -7,11 +7,25 @@ let execute lexbuf verbose =
     TypeAST.type_program ast;
     if verbose then AST.print_program ast
   with
+  	| CheckAST.Variable_exists(elem)                         -> CheckAST.print_variable_exists elem
+  	| CheckAST.Unknown_variable(elem)                        -> CheckAST.print_unknown_variable elem
+  	| CheckAST.Attribute_exists(attr)                        -> CheckAST.print_attribute_exists attr
+  	| CheckAST.Unknown_attribute(attr)                       -> CheckAST.print_unknown_attribute attr
+  	| CheckAST.Method_exists(met)                            -> CheckAST.print_method_exists met
+  	| CheckAST.Unknown_method(met)                           -> CheckAST.print_unknown_method met
+  	| CheckAST.Class_exists(c)                               -> CheckAST.print_class_exists c
+  	| CheckAST.Unknown_class(c)                              -> CheckAST.print_unknown_class (String.concat "." c)
+  	| CheckAST.Wrong_types_declaration(x, y)                 -> CheckAST.print_wrong_types_declaration x y
+  	| CheckAST.Wrong_type_unitary_operation(operator, x)     -> CheckAST.print_wrong_type_unitary_operation operator x
     | CheckAST.Wrong_types_operation(x, operator, y)         -> CheckAST.print_wrong_types_operation x operator y
 		| CheckAST.Wrong_types_assign_operation(x, operator,  y) -> CheckAST.print_wrong_types_assign_operation x operator y    
     | CheckAST.Wrong_type_bool(b)                            -> CheckAST.print_wrong_type_bool b
     | CheckAST.Wrong_types_bool(b1, b2)                      -> CheckAST.print_wrong_types_bool b1 b2
     | CheckAST.Wrong_type_if(elem)                           -> CheckAST.print_wrong_type_if elem
+    | CheckAST.Wrong_type_post(elem)                         -> CheckAST.print_wrong_type_post elem
+    | CheckAST.Wrong_type_list(elem1, elem2)                 -> CheckAST.print_wrong_type_list elem1 elem2
+    | CheckAST.Wrong_type_return(x, y)                       -> CheckAST.print_Wrong_type_return x y
+    | CheckAST.Return_expression_no_type                     -> print_endline "Syntax error on return type"
     | Error ->
       print_string "Syntax error: ";
       Location.print (Location.curr lexbuf)
