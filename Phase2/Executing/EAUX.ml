81c397143deb48bab88ae83c25c7f1d815cfb57c
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-03-11 16:36:27 +0100

    executing call statement

--- Phase2/Executing/EAUX.ml
+++ Phase2/Executing/EAUX.ml
@@ -27,10 +27,26 @@ let rec evaluate_expression globalScope scope expr = match expr.edesc with
 
   | Attr(exp, str) -> let value = evaluate_expression globalScope scope exp in (match value with VName(name) -> VAttr(name, str))
 
+  | Call(Some(exp), str, l) -> (match exp.edesc with Name(id) ->
+  													 		let o = find_object_in_heap globalScope.heap (Hashtbl.find (List.nth scope.scopelist  scope.currentscope) (id)) in 
+																(match o with ObjectDescriptor(od) -> 
+																	let cd = Hashtbl.find globalScope.data.classDescriptorTable od.object_type 
+																	and params_evaluated = eval_param_functions globalScope scope l []
+																	and lastobject = scope.currentobject 
+																	and lastClassScoped = globalScope.currentClassScoped in
+			 													(match cd with ClassDescriptor(cdescr) -> 
+			 														let m = Hashtbl.find globalScope.data.methodTable (Hashtbl.find cdescr.class_methods str) in  
+																	globalScope.currentClassScoped <- od.object_type; scope.currentscope <- scope.currentscope +1;
+																	scope.scopelist <- scope.scopelist @ [Hashtbl.create 20]; scope.currentobject <- o;
+																	let returnvalue = exec_met m globalScope scope params_evaluated in  
+																	globalScope.currentClassScoped <- lastClassScoped; scope.scopelist <- remove_at scope.currentscope scope.scopelist; 
+																	scope.currentscope <- scope.currentscope-1; scope.currentobject <- lastobject;  returnvalue )))
+
+  | Call(None, str, l) -> let m = Hashtbl.find globalScope.data.methodTable (globalScope.currentClassScoped ^ "_" ^ str) in exec_met m globalScope scope; VNull
+
   | VoidClass -> VNull
 
   | ClassOf t -> VNull
-
 
 and eval_operator operator elem1 elem2 globalScope scope = 
 	match operator, elem1, elem2 with
@@ -70,7 +86,7 @@ and eval_assign_operator operator elem1 elem2 globalScope scope =
   | Assign -> (match elem1, elem2 with
 					| VName(name), _         -> replace_execvalue_in_scope globalScope  scope name elem2; elem2
 					| VAttr(oname, aname), _ -> replace_attribute_value globalScope scope oname aname elem2; elem2
-  						)
+  )
   | Ass_add -> eval_assign_operator Assign elem1 (eval_operator Op_add elem1 elem2 globalScope scope) globalScope scope 
   | Ass_sub -> eval_assign_operator Assign elem1 (eval_operator Op_sub elem1 elem2 globalScope scope) globalScope scope
   | Ass_mul -> eval_assign_operator Assign elem1 (eval_operator Op_mul elem1 elem2 globalScope scope) globalScope scope
@@ -110,3 +126,29 @@ and eval_pre_operator operator elem1 globalScope scope =
   | Op_decr, VName(name) -> let execvalue = find_execvalue_in_scope scope name in 
   														(match execvalue with VInt(i) -> 
   															replace_execvalue_in_scope globalScope  scope name (VInt(i-1)); VInt(i))
+
+and exec_met met globalScope scope params_evaluated =
+	 print_endline("executing method" ^ met.mname ^"");
+	 List.iter2 (addParameterToScope globalScope scope) met.margstype params_evaluated;
+	 try
+	 	List.iter (evaluate_statement globalScope scope) met.mbody;
+		VNull
+		with ReturnStatementException(execution_value) -> 
+			match execution_value with
+				| VName(vname)                  -> find_execvalue_in_scope scope vname
+				| VAttr(object_name, attr_name) -> find_attribute_value globalScope scope object_name attr_name
+				| _                             -> execution_value
+
+
+and addParameterToScope globalScope scope marg param_evaluated  =
+  add_variable_to_scope globalScope scope marg.pident param_evaluated
+
+
+and eval_param_functions globalScope scope params params_evaluated = 
+  match params with
+  | [] -> params_evaluated
+  | (param::liste)  -> let execvalue = (evaluate_expression globalScope scope param) in 
+                        (match execvalue with 
+                          | VName(name) -> find_execvalue_in_scope scope name
+                          | VAttr(oname, aname) -> find_attribute_value globalScope scope oname aname
+                          | _ -> execvalue) :: eval_param_functions globalScope scope liste params_evaluated
